e65184ca130c494d51c2404e18429107a2cbb423
alphons-john <alphons.john@jobinandjismi.com>
2025-06-19 14:39:01 +0530

    OTO-8905-ci-format structure(WIP)

--- src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-8905-3rd-Party-Objects/jj_rl_third_party_application_otp-8905.js
+++ src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-8905-3rd-Party-Objects/jj_rl_third_party_application_otp-8905.js
@@ -16,9 +16,9 @@ ${OTP-8905}:{Send Sales Order Details to 3rd party application}
 *
 *Date Created:10-June-2025
 *
-*Description:Develop multiple APIs for third-party application integration. Implement Token-Based Authentication to ensure secure
-*and controlled access. APIs will handle fetching, creating, updating, and deleting sales orders and fulfillments while maintaining
-*data integrity and security.
+*Description:Develop multiple APIs for third-party application integration. Implement Token-Based
+*Authentication to ensure secure and controlled access. APIs will handle fetching, creating, updating,
+*and deleting sales orders and fulfillments while maintaining data integrity and security.
 *
 ** REVISION HISTORY
  *
@@ -29,11 +29,13 @@ define(['N/record', 'N/search', 'N/log'],
 
          /**
          * Defines the function that is executed when a POST request is sent to a RESTlet.
-         * @param {string | Object} requestBody - The HTTP request body; request body is passed as a string when request
-         *     Content-Type is 'text/plain' or parsed into an Object when request Content-Type is 'application/json' (in which case
+         * @param {string | Object} requestBody - The HTTP request body; request body is passed 
+         * as a string when request Content-Type is 'text/plain' or parsed into an Object when 
+         * request Content-Type is 'application/json' (in which case
          *     the body must be a valid JSON)
-         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
-         *     Object when request Content-Type is 'application/json' or 'application/xml'
+         * @returns {string | Object} HTTP response body; returns a string when request Content-Type
+         *  is 'text/plain'; returns an Object when request Content-Type is 'application/json' or 
+         * 'application/xml'
          * @since 2015.2
          * ${OTP-8907}:{Create API for creating the Item Fulfillment}
          */
@@ -43,9 +45,11 @@ define(['N/record', 'N/search', 'N/log'],
 
         /**
          * Defines the function that is executed when a GET request is sent to a RESTlet.
-         * @param {Object} requestParams - Parameters from HTTP request URL; parameters passed as an Object (for all supported
+         * @param {Object} requestParams - Parameters from HTTP request URL; parameters passed
+         *  as an Object (for all supported
          *     content types)
-         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
+         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 
+         * 'text/plain'; returns an
          *     Object when request Content-Type is 'application/json' or 'application/xml'
          * @since 2015.2
          * ${OTP-8906}:{Create API for the fetching the Sales order details}
@@ -61,10 +65,13 @@ define(['N/record', 'N/search', 'N/log'],
 
         /**
          * Defines the function that is executed when a PUT request is sent to a RESTlet.
-         * @param {string | Object} requestBody - The HTTP request body; request body are passed as a string when request
-         *     Content-Type is 'text/plain' or parsed into an Object when request Content-Type is 'application/json' (in which case
+         * @param {string | Object} requestBody - The HTTP request body; request body are passed as 
+         * a string when request
+         *     Content-Type is 'text/plain' or parsed into an Object when request Content-Type is 
+         * 'application/json' (in which case
          *     the body must be a valid JSON)
-         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
+         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 
+         * 'text/plain'; returns an
          *     Object when request Content-Type is 'application/json' or 'application/xml'
          * @since 2015.2
          * ${OTP-8908}:{Create API for updating the Item Fulfillment}
@@ -75,9 +82,11 @@ define(['N/record', 'N/search', 'N/log'],
 
         /**
          * Defines the function that is executed when a DELETE request is sent to a RESTlet.
-         * @param {Object} requestParams - Parameters from HTTP request URL; parameters are passed as an Object (for all supported
+         * @param {Object} requestParams - Parameters from HTTP request URL; parameters are passed as an
+         *  Object (for all supported
          *     content types)
-         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
+         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is
+         *  'text/plain'; returns an
          *     Object when request Content-Type is 'application/json' or 'application/xml'
          * @since 2015.2
          * ${OTP-8909}:{API for the Deleting the Item fulfillment}
